33077710381cbf9b9777b88cdfbb7b741253e8dc
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-12-19 23:05:35 +0530

    add environment configuration, user model, and Cloudinary integration; refactor user routes and authentication middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { uploadProcessData } from "./firebase.js";
+
 const app = express();
 
 app.use(cors({
@@ -17,10 +17,6 @@ app.get("/", (_req, res) => {
     res.json({ message: "Hello World!" });
 });
 
-app.get("/upload", async (_req, res) => {
-    await uploadProcessData();
-    res.json({ message: "success" });
-});
 
 import userRoutes from "./routes/user.routes.js";
 app.use("/api/v1/users", userRoutes)
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,11 @@
 import { config } from "dotenv";
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
-import { initializeFirebaseApp } from "./firebase.js";
 
 config({
     path: "./.env"
 });
-initializeFirebaseApp()
+
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8001, () => {
